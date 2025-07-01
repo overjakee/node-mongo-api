f53c3c309473e5978c0ad4605e69ae7de8a3ebeb
Jaki Patripkiree <jaki@sense-infotech.com>
2025-07-01 14:43:30 +0700

    edit package

--- src/server.js
+++ src/server.js
@@ -1,16 +1,10 @@
 require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
+const app = require('./app');
+const connectDB = require('./config/db');
 
-const app = express();
-app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB error:", err));
+connectDB();
 
 app.get("/", (req, res) => res.send("Hello from API"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
